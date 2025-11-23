218303508c970ce495327cf5dcc5e47f3a4db73b
Enigma <626201515@qq.com>
2025-11-23 15:36:51 +0800

    Update state.py

--- state.py
+++ state.py
@@ -465,7 +465,7 @@ def motion_state_smooth(real_state,timestamp,time_thre,len_win=5): #a sliding wi
                             if i+k<0:
                                 continue
                             if k>0 and np.abs(timestamp[i+k]-timestamp[i])>=time_thre:
-                                if k==1 and real_state[i-2]!=real_state[i-3] and i>=3:
+                                if k==1 and i>=3 and real_state[i-2]!=real_state[i-3]:
                                     #continue, you can try to skip this step
                                     diff_count=0
                                     same_count=0
@@ -495,7 +495,7 @@ def motion_state_smooth(real_state,timestamp,time_thre,len_win=5): #a sliding wi
                         #print(i+k,k,"skip")
                         continue
                     if k>0 and np.abs(timestamp[i+k]-timestamp[i])>=time_thre:
-                        if k==1 and real_state[i-2]!=real_state[i-3] and i>=3:
+                        if k==1 and i>=3 and real_state[i-2]!=real_state[i-3]:
                             #continue, you can try to skip this step
                             diff_count=0
                             same_count=0
