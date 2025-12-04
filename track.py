2f095bb738635ab3c4d11a2ea98c8b606c400b51
Enigma <626201515@qq.com>
2025-12-04 22:13:38 +0800

    Update track.py

--- track.py
+++ track.py
@@ -265,7 +265,7 @@ class TrackTree():
                 print(bottom_node.parent.obj.timestamp,bottom_node.parent.obj.positions,bottom_node.parent.obj.state_label)
                 print(" ")
                 
-            check_none_flag=False
+            check_none_flag=True # if you want to ignore the trace where all subjects are none (false detection), you can set it as False. This paper evaluates the detection and recognition seperately so the confusion metric shows the accuracy of detected subjects and the miss and false detection is evaluated seperately. A better way to is combine two metric together.
             temp_child=bottom_node
             trace_single=[]
             while temp_child!=None:
